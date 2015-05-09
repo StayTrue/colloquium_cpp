bb39699cb8d2ef6c86572d3153d8f6661b1ba1af
StayTrue <staytrueanddiefast@gmail.com>
2015-05-09 10:09:56 +0300

    Update TRANZ_Z_Q.cpp

--- modules/fractional/TRANZ_Z_Q.cpp
+++ modules/fractional/TRANZ_Z_Q.cpp
@@ -1,9 +1,13 @@
 // Copyright 2015 © Группа 4373, Митряйкин Максим и Бойков Кирилл, 26.04.2015
+#ifndef TRANS_Z_Q_CPP
+#define TRANS_Z_Q_CPP
 
 #include "../../structs/struct.h"
 	
-fraction TRANZ_Z_Q(const &integer Z) {
+fraction TRANS_Z_Q(const &integer Z) {
 	fraction Q;
 	Q.numerator = Z; // Присваиваем числителю нашего рационального числа значение целого, хранящегося в переменной Z
 	return Q; // Возвращаем значение рационального числа
 }
+
+#endif //TRANS_Z_Q_CPP
